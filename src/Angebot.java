67870516204fef06f11a03137026399f3171e352
lionel.westreicher <lionel.westreicher@gmail.com>
2024-10-10 15:23:36 +0200

    Die Klasse Angebot machtr die anderen Klassen erst nützbar. Sie hat verschiedene Parameter: flugnummer, flugdatum, regulärerPreis, rabattstrategie, reduziertenpreis. Im Konstrukter wird die Methode getrabattstrategie aufgerufen. Die Methode nutzt Monatvalue und ordnet sie einem int zu, dann wird gecheckt welches monat dem rabattmonat entspricht. ebenfalls wird getReduzierten preis iM Konstruktor aufgerufen das nimmt dann die zugeordnete rabattstrategie und führt dann die methode getreduziertenpreis  vom jeweiligen Rabatt aus.

---
+++ src/Angebot.java
@@ -0,0 +1,65 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
+public class Angebot {
+    private double flugnummer;
+    private LocalDate flugdatum;
+    private double regulärerPreis;
+    Rabattstrategie rabattstrategie;
+    private double reduziertenpreis;
+
+
+    public Angebot(double flugnummer, LocalDate flugdatum, double regulärerPreis) {
+        this.flugnummer = flugnummer;
+        this.flugdatum = flugdatum;
+        this.regulärerPreis = regulärerPreis;
+        getRabattstrategie();
+         reduziertenpreis = getReduziertenpreis();
+
+    }
+
+    public void getRabattstrategie() {
+        int monatvlue=flugdatum.getMonthValue();
+        if (monatvlue==1||monatvlue==4||monatvlue==10) {
+            this.rabattstrategie=new MaxiDiscount();
+        }
+        else if (monatvlue==2||monatvlue==3) {
+            this.rabattstrategie=new MidDiscount();
+        }
+        else {
+            this.rabattstrategie= new ZeroDiscount();
+        }
+
+    }
+    public double getReduziertenpreis(){
+       return rabattstrategie.getReduzierterPreis(regulärerPreis);
+    }
+
+
+    public double getFlugnummer() {
+        return flugnummer;
+    }
+
+    public void setFlugnummer(double flugnummer) {
+        this.flugnummer = flugnummer;
+    }
+
+    public LocalDate getFlugdatum() {
+        return flugdatum;
+    }
+
+    public void setFlugdatum(LocalDate flugdatum) {
+        this.flugdatum = flugdatum;
+    }
+
+    public double getRegulärerPreis() {
+        return regulärerPreis;
+    }
+
+    public void setRegulärerPreis(double regulärerPreis) {
+        this.regulärerPreis = regulärerPreis;
+    }
+    
+}
